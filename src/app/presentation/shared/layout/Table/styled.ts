543f7b4f46bcca8d35caabd4ae0c55ac9809c345
eusamir <samir.andrade@ceuma.br>
2024-09-16 02:24:38 -0300

    feat: header com filtro de pesquisa

--- src/app/presentation/shared/layout/Table/styled.ts
+++ src/app/presentation/shared/layout/Table/styled.ts
@@ -2,6 +2,38 @@
 
 import { styled } from 'styled-components'
 
+interface typeStatus {
+  $status: string
+}
+
+const backgroundColor = (status:string) => {
+  switch (status) {
+    case 'Operando':
+      return '#C9F1E8';
+    case 'Manutenção':
+      return '#FFEFD6';
+    case 'Parado':
+      return '#FFE2E7';
+    default:
+      return 'gray'; // cor padrão caso o status não seja reconhecido
+  }
+};
+
+const colorText = (status:string) => {
+  switch (status) {
+    case 'Operando':
+      return '#008767';
+    case 'Manutenção':
+      return '#A44900';
+    case 'Parado':
+      return '#BE1634';
+    default:
+      return 'gray'; // cor padrão caso o status não seja reconhecido
+  }
+};
+
+
+
 export const Table = styled.table`
   border-collapse: collapse;
   border-spacing: 0;
@@ -52,7 +84,6 @@ export const RowHeader = styled.tr`
 export const Data = styled.td`
   padding: 1.9rem 3.5rem;
   white-space: nowrap;
-  overflow: hidden;
   text-overflow: ellipsis;
 
   display: flex;
@@ -71,6 +102,10 @@ export const ActionColumnTD = styled(Data)`
   color: #3A4559;
   font-weight: 500;
   font-size:2rem;
-
 `
-
+export const Tag = styled.div<typeStatus>`
+  background-color: ${(props) => backgroundColor(props.$status)};
+  padding: 0.8rem;
+  border-radius: 1rem;
+  color: ${(props) => colorText(props.$status)};
+`
