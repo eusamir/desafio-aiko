d883215598d19fc03373325a7827aa00c56b6267
eusamir <samir.andrade@ceuma.br>
2024-09-16 15:00:15 -0300

    feat: rota para pegar todos os equipamentos

---
+++ src/app/api/locations/route.ts
@@ -0,0 +1,29 @@
+import dataLocation from '../../../../data/equipmentPositionHistory.json'
+import dataEquipment from '../../../../data/equipment.json'
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+
+  // Encontrando o equipamento com base no id
+  const findLocationEquipments = dataLocation.map(equipment => {
+    const equipmentsLocations = equipment.positions[equipment.positions.length -1]
+
+    const name = dataEquipment.filter(equipmentData => equipmentData.id === equipment.equipmentId)
+
+    return {
+        location: equipmentsLocations,
+        nameEquipment: name.map(e=> e.name)
+    }
+})
+
+
+  // Verificando se o equipamento foi encontrado
+  if (!findLocationEquipments) {
+    return NextResponse.json({ message: 'Product not found.' }, { status: 400 })
+  }
+
+  // Retornando o equipamento encontrado
+  return NextResponse.json(findLocationEquipments)
+}
