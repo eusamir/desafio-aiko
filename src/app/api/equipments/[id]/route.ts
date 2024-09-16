0c3920729cdb864497a73e490745f0e0d5d11e7c
eusamir <samir.andrade@ceuma.br>
2024-09-16 02:22:42 -0300

    feat: criando logica de rotas para retorno cruzado de dados

---
+++ src/app/api/equipments/[id]/route.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod';
+import data from '../../../../../data/equipmentStateHistory.json';
+import earnysPerHour from '../../../../../data/equipmentModel.json';
+import statesTypes from '../../../../../data/equipmentState.json';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  _: Request,
+  { params }: { params: { id: string } }
+) {
+  // Simulação de atraso para requisição
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  // Validação do ID
+  const id = z.string().nullish().parse(params.id);
+
+  // Filtrando os dados de histórico de estado pelo ID do equipamento
+  const getDataFromId = data.filter((e) => e.equipmentId === id);
+
+  // Criando a fusão entre os estados do equipamento e os tipos de estado
+  const MergedEquipments = getDataFromId.map((equipment) => {
+    // Mapeando os estados com seus tipos
+    const mappedStates = equipment.states.map((state) => {
+      const stateType = statesTypes.find((e) => e.id === state.equipmentStateId);
+
+      const priceOperating = earnysPerHour.find((e) => e.name);
+
+      const teste = priceOperating?.hourlyEarnings.find(e=> e.equipmentStateId === state.equipmentStateId)
+
+      return {
+        ...state,
+        activeState: stateType?.name,
+        price: teste
+      };
+    });
+
+    // Retornando o equipamento com os estados mapeados
+    return {
+      ...equipment,
+      states: mappedStates,
+    };
+  });
+
+  // Retornando o equipamento encontrado
+  return NextResponse.json(MergedEquipments);
+}
---
+++ src/app/api/locations/[id]/route.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod'
+import dataLocation from '../../../../../data/equipmentPositionHistory.json'
+import { NextResponse } from 'next/server'
+
+export async function GET(
+  _: Request,
+  { params }: {params:{id:string}},
+) {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  // Validação do ID
+  const id = z.string().nullish().parse(params.id)
+
+  // Encontrando o equipamento com base no id
+  const findLocationEquipments = dataLocation.find(equipment => {
+    return equipment.equipmentId === id
+})
+
+
+  // Verificando se o equipamento foi encontrado
+  if (!findLocationEquipments) {
+    return NextResponse.json({ message: 'Product not found.' }, { status: 400 })
+  }
+
+  // Retornando o equipamento encontrado
+  return NextResponse.json(findLocationEquipments)
+}
