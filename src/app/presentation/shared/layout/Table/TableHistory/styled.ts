b16a984972fabc98197beaa076870c5af2c0e894
eusamir <samir.andrade@ceuma.br>
2024-09-16 02:27:47 -0300

    style: estilização do tableHistory

---
+++ src/app/presentation/shared/layout/Table/TableHistory/styled.ts
@@ -0,0 +1,111 @@
+'use client'
+
+import { styled } from 'styled-components'
+
+interface typeStatus {
+  $status: string
+}
+
+const backgroundColor = (status:string) => {
+  switch (status) {
+    case 'Operando':
+      return '#C9F1E8';
+    case 'Manutenção':
+      return '#FFEFD6';
+    case 'Parado':
+      return '#FFE2E7';
+    default:
+      return 'gray'; // cor padrão caso o status não seja reconhecido
+  }
+};
+
+const colorText = (status:string) => {
+  switch (status) {
+    case 'Operando':
+      return '#008767';
+    case 'Manutenção':
+      return '#A44900';
+    case 'Parado':
+      return '#BE1634';
+    default:
+      return 'gray'; // cor padrão caso o status não seja reconhecido
+  }
+};
+
+
+
+export const Table = styled.table`
+  border-collapse: collapse;
+  border-spacing: 0;
+
+  width: 70%;
+  table-layout: fixed;
+  color: #B3B2B8;
+  border: 1px solid #E1E1E1;
+
+  tr {
+    display: flex;
+  }
+`
+
+export const Head = styled.thead`
+  border-top: none;
+`
+export const TableBody = styled.tbody``
+
+export const Header = styled.th`
+  padding: 1.9rem 3.5rem;
+  white-space: nowrap;
+
+  display: flex;
+  align-items: center;
+  flex: 0 0 auto;
+  width: 20%;
+  background: #F0F0F0;
+  border: 1px solid #F0F0F0;
+  font-size: 2rem;
+  font-weight: 700;
+`
+export const Row = styled.tr`
+  background: white;
+  border-top: 1px solid #E1E1E1;
+  font-size: 2rem;
+  font-weight: 400;
+  justify-content: space-between;
+  cursor: pointer;
+`
+export const RowHeader = styled.tr`
+  background: white;
+  border-top: 1px solid #E1E1E1;
+  font-size: 2rem;
+  font-weight: 400;
+  justify-content: space-between;
+`
+export const Data = styled.td`
+  padding: 1.9rem 3.5rem;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  display: flex;
+  align-items: center;
+  flex: 0 0 auto;
+  width: 20%;
+  font-weight: 500;
+  color: black;
+`
+
+export const ActionColumnTH = styled(Header)`
+  gap: 1.2rem;
+`
+export const ActionColumnTD = styled(Data)`
+  gap: 1.2rem;
+  color: #3A4559;
+  font-weight: 500;
+  font-size:2rem;
+`
+export const Tag = styled.div<typeStatus>`
+  background-color: ${(props) => backgroundColor(props.$status)};
+  padding: 0.8rem;
+  border-radius: 1rem;
+  color: ${(props) => colorText(props.$status)};
+`
