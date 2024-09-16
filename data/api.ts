a23852d55cdce6cc8774ea5a7758eae49b494ed4
eusamir <samir.andrade@ceuma.br>
2024-09-16 02:22:11 -0300

    feat:  criando função de auxilio para chamadas api

---
+++ data/api.ts
@@ -0,0 +1,9 @@
+export function api(path: string, init?: RequestInit) {
+    const baseUrl = 'http://localhost:3000';
+    const apiPrefix = '/api';
+  
+    const url = new URL(`${apiPrefix}${path}`, baseUrl);
+  
+    return fetch(url.toString(), init);
+  }
+  
