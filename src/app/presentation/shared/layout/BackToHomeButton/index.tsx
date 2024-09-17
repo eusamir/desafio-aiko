232a6ba3ab7458d7ceed124a943e8ae34c95aff7
eusamir <samir.andrade@ceuma.br>
2024-09-17 01:06:59 -0300

    feat: chamado para voltar a home

---
+++ src/app/presentation/shared/layout/BackToHomeButton/index.tsx
@@ -0,0 +1,14 @@
+import { CaretLeft } from "@phosphor-icons/react/dist/ssr"
+import Link from "next/link"
+import * as S from './styled'
+
+export default function BackToHomeButton(){
+    return(
+        <S.Container>
+            <Link href={"/"}>
+                <CaretLeft size={32} color="#003184" />
+            </Link>
+            <h1>Inicio</h1>
+        </S.Container>
+    )
+}
--- src/app/presentation/shared/layout/Header/index.tsx
+++ src/app/presentation/shared/layout/Header/index.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
 import * as S from './styled'
-import Link from "next/link";
 
 export default function Header(){
   return(
     <S.Container>
-      <Link href={'/'}>
         <Image src={'/images/aiko.png'} alt="" width={122} height={62}/>
-      </Link>
     </S.Container>
   )
 }
